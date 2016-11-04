839dcf8fe0ad3f6035f74ef7067624311e195d38
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2016-11-04 23:33:26 +0900

    start packaging

---
+++ setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from codecs import open
+
+
+def readme():
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+setup(
+    name='fitness_crawler',
+    version='0.0.1',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url='https://github.com/kaist-irnlp/ekp_fitness_crawler',
+    license='',
+    description='Fitness resource crawler',
+    long_description=readme(),
+    author='Kyoung-Rok Jang',
+    author_email='kyoungrok.jang@gmail.com',
+    install_requires=[
+        'scrapy',
+    ],
+    entry_points='''
+        [console_scripts]
+        fitness_crawler=fitness_crawler.__main__:main
+    ''',
+    zip_safe=False
+)
